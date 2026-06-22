0fcbd51d65fa8dd05a50c2747188051e1746d758
Guillermina Fiore <guillefiore03@gmail.com>
2026-06-22 11:00:51 -0300

    update: main.tf .gitignore

--- main.tf
+++ main.tf
@@ -7,4 +7,54 @@ provider "aws" {
 resource "aws_instance" "nginx-server" {
 	ami = "ami-0440d3b780d96b29d"
 	instance_type = "t3.micro"
+
+	# use yum because the AMI is Amazon Linux 2
+	user_data = <<-EOF
+						#!/bin/bash
+						sudo yum install -y nginx 
+						sudo systemctl enable nginx
+						sudo systemctl start nginx
+						EOF
+
+	key_name = aws_key_pair.nginx-server-ssh.key_name
+	
+	vpc_security_group_ids = [
+		aws_security_group.nginx-server-sg.id
+	]
+}
+
+# create a key pair for SSH 
+resource "aws_key_pair" "nginx-server-ssh" {
+	key_name = "nginx-server-ssh"
+	public_key = file("nginx-server.key.pub")
+}
+
+# create a security group 
+resource "aws_security_group" "nginx-server-sg" {
+	name = "nginx-server-sg"
+	description = "Security group allowing HTTP and SSH access"
+
+	# allow SSH
+	ingress {
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	# allow HTTP
+	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	# allow outbound traffic
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1" # all protocols
+		cidr_blocks = ["0.0.0.0/0"]
+	}
 }
